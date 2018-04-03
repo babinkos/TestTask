9bff35f3a6e1b35cbf83aaa1358b96b10059d22d
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-03 15:54:23 +0300

    solving ssh pubkey issues (error on first connect : The authenticity of host '..' can't be established.)

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DBIP="192.168.56.102"
+WEBIP=$DBIP
 sudo sed -i 's%archive.ubuntu.com%mirror.yandex.ru%' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -9,9 +11,16 @@ sudo apt-get update
 sudo apt-get install ansible -y
 ansible-galaxy install geerlingguy.mysql
 echo "[db-servers]" | sudo tee /etc/ansible/hosts
-echo "192.168.56.102" | sudo tee --append /etc/ansible/hosts
+echo "$DBIP" | sudo tee --append /etc/ansible/hosts
 echo "[web-servers]" | sudo tee --append /etc/ansible/hosts
-echo "192.168.56.102" | sudo tee --append /etc/ansible/hosts
+echo "$WEBIP" | sudo tee --append /etc/ansible/hosts
+
+# adding second VM ssh pubkeys to solve unattended ssh
+if [ -z `ssh-keygen -F $DBIP` ]; then
+  ssh-keyscan -H $DBIP >> ~/.ssh/known_hosts
+fi
+
+# to be uncommented after tests
 #sudo apt-get install git
 #git clone -b dev https://github.com/babinkos/TestTask.git
 #cd /TestTask/jboss-guestbook
