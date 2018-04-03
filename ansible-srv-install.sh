4653f10a2be19428651649177851fae764fc06c1
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-03 11:44:59 +0300

    fix: sudo is not needed for command : ansible-galaxy install geerlingguy.mysql
    as a matter of fact that sudo command is a way to become root privileges, using sudo will cause deployment of roles from ansible-galaxy to current user profile - that is root but not vagrant as expected. So we need to remove sudo and simple run install command for role. Also kernel and console log of VM contain a lot of sensitive data and should be added to .gitignore

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -7,7 +7,7 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
-sudo ansible-galaxy install geerlingguy.mysql
+ansible-galaxy install geerlingguy.mysql
 echo "[db-servers]" | sudo tee /etc/ansible/hosts
 echo "192.168.56.102" | sudo tee --append /etc/ansible/hosts
 echo "[web-servers]" | sudo tee --append /etc/ansible/hosts
