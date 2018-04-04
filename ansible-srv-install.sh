1aaa6a3818a46550b1fcf5ebe4460b7cf35038d6
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-04 22:21:16 +0300

    jboss user management: using become_user and become_method

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -30,7 +30,7 @@ git init
 git remote add -f origin https://github.com/babinkos/TestTask.git
 git config core.sparseCheckout true
 echo "jboss-guestbook/*" >> .git/info/sparse-checkout
-git pull origin master
+git pull origin dev
 git checkout dev
 sudo chown -R vagrant:vagrant /home/vagrant/TestTask 
 ansible-playbook /home/vagrant/TestTask/jboss-guestbook/site.yml
