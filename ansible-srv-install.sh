0ffd5a0af905c0bbfe61d6a239d6e70faec90656
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-04 22:14:44 +0300

    jboss dir owner: added become again

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -2,13 +2,13 @@
 DBIP="192.168.56.102"
 WEBIP=$DBIP
 sudo sed -i 's%archive.ubuntu.com%mirror.yandex.ru%' /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install software-properties-common -y
-sudo apt-get install python-software-properties -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install software-properties-common -y
+sudo apt install python-software-properties -y
 sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt update
+sudo apt install ansible -y
 ansible-galaxy install geerlingguy.mysql
 echo "[db-servers]" | sudo tee /etc/ansible/hosts
 echo "$DBIP" | sudo tee --append /etc/ansible/hosts
