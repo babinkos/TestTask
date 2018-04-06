929081857c484e6ec5ab7d115a0031a616a7f38e
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-06 17:42:43 +0300

    /etc/ansible/hosts [all:vars] fix for:Failed to connect to the host via ssh: Warning: Permanently added (RSA) to the list of known hosts. Permission denied (publickey)

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -12,7 +12,9 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 ansible-galaxy install geerlingguy.mysql
-echo "[db-servers]" | sudo tee /etc/ansible/hosts
+echo "[all:vars]" | sudo tee /etc/ansible/hosts
+echo "ansible_ssh_common_args='-o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa'" | sudo tee --append /etc/ansible/hosts
+echo "[db-servers]" | sudo tee --append /etc/ansible/hosts
 echo "$DBIP ansible_user=vagrant" | sudo tee --append /etc/ansible/hosts
 echo "[web-servers]" | sudo tee --append /etc/ansible/hosts
 echo "$WEBIP ansible_user=vagrant" | sudo tee --append /etc/ansible/hosts
@@ -24,5 +26,6 @@ git remote add -f origin https://github.com/babinkos/TestTask.git
 git config core.sparseCheckout true
 echo "jboss-guestbook/*" >> .git/info/sparse-checkout
 git pull origin dev
-sudo chown -R vagrant:vagrant /home/vagrant/TestTask 
-ansible-playbook /home/vagrant/TestTask/jboss-guestbook/site.yml
+sudo chown -R vagrant:vagrant /home/vagrant/TestTask
+ansible all -m ping -v
+ansible-playbook /home/vagrant/TestTask/jboss-guestbook/site.yml -v
