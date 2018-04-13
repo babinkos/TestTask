90e61fe74cf5e7b1cd1c44a92f769ca9b73d213c
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-13 15:08:59 +0300

    added requirements and dependencies for mysql role

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -18,7 +18,7 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
-ansible-galaxy -c -v install geerlingguy.mysql
+#ansible-galaxy -c -v install geerlingguy.mysql
 echo "[all:vars]" | sudo tee /etc/ansible/hosts
 echo "ansible_ssh_common_args='-o StrictHostKeyChecking=yes -o HostKeyAlgorithms=ssh-rsa'" | sudo tee --append /etc/ansible/hosts
 echo "[db-servers]" | sudo tee --append /etc/ansible/hosts
@@ -35,5 +35,6 @@ echo "jboss-guestbook/*" >> .git/info/sparse-checkout
 git pull origin dev
 sudo chown -R vagrant:vagrant /home/vagrant/TestTask
 #ssh -qv -o BatchMode=yes -o StrictHostKeyChecking=no -o HostKeyAlgorithms=ssh-rsa -o ConnectTimeout=10 vagrant@$DBIP "ip addr;exit"
+ansible-galaxy install -c -v -r /home/vagrant/TestTask/requirements.yml
 ansible all -m ping -v
 ansible-playbook /home/vagrant/TestTask/jboss-guestbook/site.yml
