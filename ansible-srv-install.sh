d67c733a133359149e1592de36bc5630206fad29
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-04-13 16:53:21 +0300

    fix: playbook fails on old ansible 2.0; debug var

--- ansible-srv-install.sh
+++ ansible-srv-install.sh
@@ -12,7 +12,7 @@ ssh-keyscan -H $DBIP >> /home/vagrant/.ssh/known_hosts
 
 sudo sed -i 's%archive.ubuntu.com%mirror.yandex.ru%' /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:ansible/ansible -y
